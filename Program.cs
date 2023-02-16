c93ec479ba59d61b315027cc5bdb107db8a4a914
admin <admin@DESKTOP-8VL5N9M>
2023-02-16 15:55:15 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_2_1
+{
+    public delegate int AddDelegate(int a, int b, int c);
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            AddDelegate aD = new AddDelegate( delegate (int x, int y, int z)
+            {
+                int res = (x + y+x)/2;
+                return res;
+            });
+            int sum = aD(6, 2, 10);
+            Console.WriteLine(sum);
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
